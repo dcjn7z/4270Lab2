d6ac2639a931e11234ba090e7bd44328cc51b399
chkz6d <60370096+chkz6d@users.noreply.github.com>
2021-02-26 18:33:24 -0600

    Update Chan's_bubble.c

--- Chan's_bubble.c
+++ Chan's_bubble.c
@@ -2,18 +2,18 @@
 
 int main()
 {
-    int array[] = {64,34,25,12,22,11,90};
+    
+    int array[] = {5,3,6,8,9,1,4,7,2};
     int n = sizeof(array)/sizeof(array[0]);
     
-    int i,j,temp;
     
-    for (i = 0; i < n - 1; i++)
+    for (int i = 0; i < n - 1; i++)
     {
-        for(j = 0; j < n - i - 1; j++)
+        for(int j = 0; j < n - i - 1; j++)
         {
             if(array[j] > array[j+1])
             {
-                temp = array[j];
+                int temp = array[j];
                 array[j] = array[j+1];
                 array[j+1] = temp;
             }
@@ -26,3 +26,24 @@ int main()
     }
     return 0;
 }
+
+/*MIPS Assembly
+[0x04000000]    addiu $t0, $zero, 5 //
+[0x04000004]    addiu $t1, $zero, 3 
+[0x04000008]    addiu $t2, $zero, 6
+[0x0400000C]    addiu $t3, $zero, 8
+[0x04000010]    addiu $t4, $zero, 9
+[0x04000014]    addiu $t5, $zero, 1
+[0x04000018]    addiu $t6, $zero, 4
+[0x0400002C]    addiu $t7, $zero, 7
+[0x04000030]    addiu $t8, $zero, 2
+[0x04000034]    
+[0x04000038]    
+[0x0400003C]    
+[0x04000040]    
+[0x04000044]    
+[0x04000048]
+[0x0400004C]
+[0x04000050]
+[0x04000054]
+© 2021 GitHub, Inc.
