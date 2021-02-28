49587f4bd09366ceb6c8b0c23e18793ce8b7c64a
dcjn7z <dcjn7z@mail.missouri.edu>
2021-02-28 17:23:22 -0600

    Vers 3.

--- assembler.c
+++ assembler.c
@@ -1,43 +1,64 @@
-void scanInstructionID(char * inputfile, char** instructionPtr){
-	FILE* fp=fopen(inputfile,"r");
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+void scanInstructionID(FILE* fp, char* instructionPtr){
+	if (fp==NULL){
+		printf("File failed\n");}
 	fscanf(fp,"%s",instructionPtr);
-	fclose(fp);
+	printf("%s\n",instructionPtr);
 	return;
 	}
 
-void convertFileToHex(char * inputfile, uint32_t * numberPtr,char * instructionID){
-	FILE* fp=fopen(inputfile,"r");
+void addRegister(char *reg,uint32_t *numberPtr,char *regID){
+	uint32_t adder = 0;
+	if(strcmp(reg,"zero")==0){
+		adder=0;}
+	else if(strcmp(reg,"a0")==0){
+		adder=4;}
 
-	if( strcmp(instructionID,"add")==0 ){
-		*numberPtr = *numberPtr+0x00000020;
-		char rs[4],rt[4],rd[4];
-		fscanf(fp,"$%s, $%s, $%s",rs,rt,rd);
-		fclose(fp);
+	if(strcmp(regID,"rs")==0){
+		adder=adder<<21;}
+	else if(strcmp(regID,"rt")==0){
+		adder=adder<<16;}
+	else{
+		adder=adder<<11;}
+	printf("%x\n",*numberPtr);
+	printf("%x\n", adder);
+	*numberPtr=*numberPtr+adder;
 
-		addRType(rs,rt,rd,numberPtr);
-	}
-	
-	if(strcmp(instructionID,"addiu")==0){
-		*numberPtr = *numberPtr + 0x24000000;
-		char rs[4], rt[4];
-		uint32_t immed=0;
-		fscanf(fp, "$%s, $%s, 0x%x",rt,rs,immed);
-		fclose(fp);
-		
-void addRegister(char *reg, int *numberPtr){
-
+	return;
 }
 void addIntermediate(int intermediate, int *numberPtr){
-
+	
 }
 void addTarget(int target,int *numberPtr){
 
 }
 
+void convertFileToHex(uint32_t *numberPtr,char * instructionID,FILE* fp){
+	char rs[4],rt[4],rd[4];
+	uint32_t immed=0;
+	uint32_t target=0;
+
+	if( strcmp(instructionID,"add")==0 ){
+		*numberPtr = *numberPtr+0x00000020;
+		fscanf(fp,"$%s, $%s, $%s",rs,rt,rd);
+		addRegister(rs,numberPtr,rs);
+		addRegister(rt,numberPtr,rt);
+		addRegister(rd,numberPtr,rd);}
+	
+	if(strcmp(instructionID,"addiu")==0){
+		*numberPtr = *numberPtr + 0x24000000;
+		fscanf(fp, "$%s, $%s, %x",rt,rs,&immed);
+		printf("%s\n%s\n%x\n",rt,rs,immed);}
+}
+
 void writeHexToFile(uint32_t hexNumber,char* outputfile){
-	FILE* fp = fopen(outputfile,"a");
-	fprintf(fp, "%x\n",hexNumber);
-	fclose(fp);
+	FILE* fp2 = fopen(outputfile,"a");
+	fprintf(fp2, "%x\n",hexNumber);
+	fclose(fp2);
 	return;
 	}
 
@@ -54,19 +75,18 @@ int main (void) {
 
 	char instructionID[6];
 	uint32_t hexInstruction=0;
+	
+	FILE* fp=fopen(inputfile,"r");
+	
+	scanInstructionID(fp,instructionID);
 
-	scanInstructionID(inputfile, &instructionID);
-
-	while (instructionID!="syscall"){
-		convertFileToHex(inputfile,instructionID);
+	while (strcmp(instructionID,"syscall")!=0){
+		convertFileToHex(&hexInstruction,instructionID,fp);
 		writeHexToFile(hexInstruction,outputfile);
-		fscanf(input,"%s",instructionID);
+		scanInstructionID(fp, instructionID);
 		}
 	
-	printf("syscall\n");
-
-	fclose(inputfile);
-
+	fclose(fp);
 	return 1;
 	}
 
