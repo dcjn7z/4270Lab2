f97b52327fb982679ef728eb407b335385dd6538
chkz6d <60370096+chkz6d@users.noreply.github.com>
2021-03-03 20:22:15 -0600

    Update Chan's_bubble.c

--- Chan's_bubble.c
+++ Chan's_bubble.c
@@ -4,12 +4,12 @@ int main()
 {
     
     int array[] = {5,3,6,8,9,1,4,7,2};
-    int n = sizeof(array)/sizeof(array[0]);
+    int n = 8;
     
     
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j = 0; j < n - i - 1; j++)
+        for(int j = 0; j < 8 - i; j++)
         {
             if(array[j] > array[j+1])
             {
@@ -28,18 +28,18 @@ int main()
 }
 
 /*MIPS Assembly
-[0x04000000]   
-                addiu $a0, $zero, 5 //
-[0x04000004]    addiu $a1, $zero, 3 
-[0x04000008]    addiu $a2, $zero, 6
-[0x0400000C]    addiu $a3, $zero, 8
-[0x04000010]    addiu $a4, $zero, 9
-[0x04000014]    addiu $a5, $zero, 1
-[0x04000018]    addiu $a6, $zero, 4
-[0x0400002C]    addiu $a7, $zero, 7
-[0x04000030]    addiu $a8, $zero, 2
-[0x04000034]    addiu $a9, $zero, 0 // set temp to 0
-                addiu $a14, $zero, 0// set n to 0
+[0x04000000]    lui   $zero, 0x0000
+                addiu $a0, $zero, 0x5 //
+[0x04000004]    addiu $a1, $zero, 0x3 
+[0x04000008]    addiu $a2, $zero, 0x6
+[0x0400000C]    addiu $a3, $zero, 0x8
+[0x04000010]    addiu $a4, $zero, 0x9
+[0x04000014]    addiu $a5, $zero, 0x1
+[0x04000018]    addiu $a6, $zero, 0x4
+[0x0400002C]    addiu $a7, $zero, 0x7
+[0x04000030]    addiu $a8, $zero, 0x2
+[0x04000034]    addiu $a9, $zero, 0x0 // set temp to 0
+                addiu $a14, $zero, 0x8// set n to 8
                 
                 
                 lui   $t1, 0x1001 //set base 0x10010000
@@ -55,13 +55,25 @@ int main()
                 sw    $a9, $t1,36 //store temp 
                 sw    $a14,$t1,40 //store n
                 
-                addiu $a10, $zero, 0x n-1?? // upper bound for i < n-1
+                addiu $a10, $zero, 0x8 // upper bound for i < 8
                 addiu $a11, $zero, 0x0 // i is initialize to 0
                 
-                addiu $a12, $zero, 0x n-i-1 // upper bound for j < n - i -1
                 addiu $a13, $zero, 0x0 // j is initialize to 0
                 
+                lw    $a0,  $t1, 0  //load first element of array
+                lw    $a1,  $t1, 4  //load second element of array
+                
+                addiu $a16, $a1, 0x0 // register second element of array to $a16
+                bgez  $a1
+                
+                addiu $a13, $zero, 0x0 // j is initialize to 0
+                
+                sub   $a15, $a14, $a11 // 8 - i
                 
                 addiu $a13, $a13,0x1 // increment j
                 
-                addiu $a11, $zero, 0x1 // increment i
+                blez  $a0,  $a1, 0x // jumps to 
+                
+                addiu $a11, $11, 0x1 // increment i
+                
+                blez  $a11, $a14, 0x //if i < 8 jumps to second for-loop
