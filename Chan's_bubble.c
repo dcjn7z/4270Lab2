43e3f1e3c66ef028f727e22c84d89de9feccc4c0
chkz6d <60370096+chkz6d@users.noreply.github.com>
2021-03-03 21:54:13 -0600

    Update Chan's_bubble.c

--- Chan's_bubble.c
+++ Chan's_bubble.c
@@ -29,50 +29,52 @@ int main()
 
 /*MIPS Assembly
 [0x04000000]    lui   $zero, 0x0000
-                addiu $a0, $zero, 0x5 //
-[0x04000004]    addiu $a1, $zero, 0x3 
-[0x04000008]    addiu $a2, $zero, 0x6
-[0x0400000C]    addiu $a3, $zero, 0x8
-[0x04000010]    addiu $a4, $zero, 0x9
-[0x04000014]    addiu $a5, $zero, 0x1
-[0x04000018]    addiu $a6, $zero, 0x4
-[0x0400002C]    addiu $a7, $zero, 0x7
-[0x04000030]    addiu $a8, $zero, 0x2
-[0x04000034]    addiu $a9, $zero, 0x0 // set temp to 0
-                addiu $a14, $zero, 0x8// set n to 8
+                addiu $t0, $zero, 0x5 //
+[0x04000004]    addiu $t1, $zero, 0x3 
+[0x04000008]    addiu $t2, $zero, 0x6
+[0x0400000C]    addiu $t3, $zero, 0x8
+[0x04000010]    addiu $t4, $zero, 0x9
+[0x04000014]    addiu $t5, $zero, 0x1
+[0x04000018]    addiu $t6, $zero, 0x4
+[0x0400002C]    addiu $t7, $zero, 0x7
+[0x04000030]    addiu $t8, $zero, 0x2
+[0x04000034]    addiu $a0, $zero, 0x0 // set temp to 0
+                addiu $a2, $zero, 0x8// set n to 8
                 
                 
-                lui   $t1, 0x1001 //set base 0x10010000
-                sw    $a0, 0x0000 ($t1)
-[0x04000038]    sw    $a1, $t1,4
-[0x0400003C]    sw    $a2, $t1,8
-[0x04000040]    sw    $a3, $t1,12
-[0x04000044]    sw    $a4, $t1,16
-[0x04000048]    sw    $a5, $t1,20
-[0x0400004C]    sw    $a6, $t1,24
-[0x04000050]    sw    $a7, $t1,28
-[0x04000054]    sw    $a8, $t1,32
-                sw    $a9, $t1,36 //store temp 
-                sw    $a14,$t1,40 //store n
+                lui   $a1, 0x1001 //set base 0x10010000
+                sw    $a0, 0x0000 ($a1)
+[0x04000038]    sw    $t1, $a1,4
+[0x0400003C]    sw    $t2, $a1,8
+[0x04000040]    sw    $t3, $a1,12
+[0x04000044]    sw    $t4, $a1,16
+[0x04000048]    sw    $t5, $a1,20
+[0x0400004C]    sw    $t6, $a1,24
+[0x04000050]    sw    $t7, $a1,28
+[0x04000054]    sw    $t8, $a1,32
+                sw    $a0, $a1,36 //store temp 
+                sw    $a2,$a1,40 //store n
                 
-                addiu $a10, $zero, 0x8 // upper bound for i < 8
-                addiu $a11, $zero, 0x0 // i is initialize to 0
+                addiu $t8, $zero, 0x8 // upper bound for i < 8
+                addiu $t9, $zero, 0x0 // i is initialize to 0
                 
-                addiu $a13, $zero, 0x0 // j is initialize to 0
+                addiu $a3, $zero, 0x0 // j is initialize to 0
                 
-                lw    $a0,  0x0000 ($t1)  //load first element of array
-                lw    $a1,  $t1, 4  //load second element of array
-                sub   $a16, $a0, $a1 // $a16 = first element - second element
-                bgez  $a16, 0x // jump to increment j
+                lw    $t0,  0x0000 ($a1)  //load first element of array
+                lw    $t1,  $a1, 4  //load second element of array
+                sub   $f4, $t0, $t1 // $f4 = first element - second element
+                bgez  $f4, 0x14 // jump to increment j
+                addiu $a0, $t0, 0x0 // temp = array[j]
+                addiu $t0, $t1, 0x0 // array[j] = array[j+1]
+                addiu $t1, $a0, 0x0 // array[j+1] = temp
                 
-                addiu $a13, $zero, 0x0 // j is initialize to 0
                 
-                sub   $a15, $a14, $a11 // 8 - i
-                
-                addiu $a13, $a13,0x1 // increment j
-                
-                blez  $a0,  $a1, 0x // jumps to 
-                
-                addiu $a11, $11, 0x1 // increment i
-                
-                blez  $a11, $a14, 0x //if i < 8 jumps to second for-loop
+                addiu $a3, $a3,0x1 // increment j
+                sub   $f4, $a2, $t9 // 8 - i
+                sub   $f5, $f4, $a3 // (8 -i) - j
+                bltz  $f5, 0x28
+                addiu $t9, $t9, 0x1 // increment i
+                bltz  $f4, 
+                addiu $v0, $zero, 0x8
+                syscall
+              
